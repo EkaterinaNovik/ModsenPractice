9a705d5c9962ab7a420133ea3dd78c87a34a83cd
cotangens <91955444+EkaterinaNovik@users.noreply.github.com>
2023-02-24 20:49:34 +0300

    update file

--- weather/src/app/app.module.ts
+++ weather/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,10 @@ import { RainComponent } from './components/rain/rain.component';
 import { SnowComponent } from './components/snow/snow.component';
 import { SunnyComponent } from './components/sunny/sunny.component';
 import { WindyComponent } from './components/windy/windy.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,12 @@ import { WindyComponent } from './components/windy/windy.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
