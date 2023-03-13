451f13f88e23b12ab53e6dabff0da20e237528f8
cotangens <91955444+EkaterinaNovik@users.noreply.github.com>
2023-03-13 12:24:49 +0300

    modified file

--- weather/src/app/store/actions/weather.action.ts
+++ weather/src/app/store/actions/weather.action.ts
@@ -1,12 +1,19 @@
-import { Action, createAction, props } from "@ngrx/store";
+import { createAction, props, Action } from "@ngrx/store";
 
 export const FETCH_WEATHER = "fetch_weather";
 export const FETCH_WEATHER_FAIL = "fetch_weather_fail";
 export const FETCH_WEATHER_SUCCESS = "fetch_weather_success";
 
 
-export const FetchWeather = createAction(FETCH_WEATHER);
+export class FetchWeather implements Action {
+    readonly type = FETCH_WEATHER;
+    constructor(public payload: any) { }
+}
 
-export const FetchWeatherFail = createAction(FETCH_WEATHER_FAIL);
 
+
+export const FetchWeatherFail = createAction(FETCH_WEATHER_FAIL, props<{ payload: any }>());
 export const FetchWeatherSuccess = createAction(FETCH_WEATHER_SUCCESS, props<{ payload: any }>());
+
+
+
