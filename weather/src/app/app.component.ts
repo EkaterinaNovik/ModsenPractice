ceb31b18b03b29cafe8a7295f662d776ee32e9ec
cotangens <91955444+EkaterinaNovik@users.noreply.github.com>
2023-03-13 12:23:05 +0300

    modified file

--- weather/src/app/app.component.ts
+++ weather/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { selectWeatherState } from './store/reducers';
+import { select, Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather';
+  constructor(private store: Store) {
+    this.store
+      .pipe(select(selectWeatherState));
+  }
 }
