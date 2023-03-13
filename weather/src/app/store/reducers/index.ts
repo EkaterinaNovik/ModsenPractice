3774df940b68aaf5e54d1e693ecb71b2905d7548
cotangens <91955444+EkaterinaNovik@users.noreply.github.com>
2023-03-13 12:26:26 +0300

    modified file

--- weather/src/app/store/reducers/index.ts
+++ weather/src/app/store/reducers/index.ts
@@ -1,1 +1,20 @@
 
+import { ActionReducerMap, createSelector, createFeatureSelector } from "@ngrx/store";
+
+import * as weatherReducer from "./weather.reducer";
+
+
+export interface State {
+    weather: weatherReducer.WeatherState
+}
+
+export const reducers: ActionReducerMap<State> = {
+    weather: weatherReducer.reducer
+}
+
+
+export const selectWeatherState = createFeatureSelector<weatherReducer.WeatherState>("weather");
+
+export const getWeatherStateData = createSelector(selectWeatherState, weatherReducer.getWeatherData);
+export const getWeatherStateLoading = createSelector(selectWeatherState, weatherReducer.getWeatherLoading);
+export const getWeatherStateLoaded = createSelector(selectWeatherState, weatherReducer.getWeatherLoaded);
